e10a1c1a5edc096622d7d5a151f6a32b45c967ab
Jay Cheng <thefallinggrace@gmail.com>
2020-03-14 15:51:11 +0800

    Learncpp 9.3: Overloading the I/O operators

--- TEST/main.cpp
+++ TEST/main.cpp
@@ -1,101 +1,118 @@
-/*
- 1c) Extra credit: the fraction 2/4 is the same as 1/2, but 2/4 is not reduced to the lowest terms. We can reduce any given fraction to lowest terms by finding the greatest common divisor (GCD) between the numerator and denominator, and then dividing both the numerator and denominator by the GCD.
+// Take the Fraction class we wrote in the previous quiz (listed below) and add an overloaded operator<< and operator>> to it.
 
- The following is a function to find the GCD:
- int gcd(int a, int b) {
-     return (b == 0) ? (a > 0 ? a : -a) : gcd(b, a % b);
- }
- Add this function to your class, and write a member function named reduce() that reduces your fraction. Make sure all fractions are properly reduced.
-*/
 #include <iostream>
- 
+
 class Fraction
 {
-public:
-    Fraction(int numerator=0, int denominator=1) : m_numerator(numerator), m_denominator(denominator)
-    { reduce(); }
-    
-    void print()
-    {
-        std::cout << m_numerator << '/' << m_denominator << '\n';
-    }
-    
-    // We'll make gcd static so that it can be part of class Fraction without requiring an object of type Fraction to use
-    static int gcd(int a, int b)
-    {
-        return (b == 0) ? (a > 0 ? a : -a) : gcd(b, a % b);
-    }
-    
-    void reduce()
-    {
-        if ( m_numerator != 0 && m_denominator !=0)
-        {
-            int gcd = Fraction::gcd(m_numerator, m_denominator);
-            m_numerator /= gcd;
-            m_denominator /= gcd;
-        }
-    }
-    
-    friend Fraction operator*(const Fraction& f1, const Fraction& f2);
-    friend Fraction operator*(const Fraction& f1, int value);
-    friend Fraction operator*(int value, const Fraction& f1);
-    
 private:
-    int m_numerator;
-    int m_denominator;
+	int m_numerator;
+	int m_denominator;
+	
+public:
+	Fraction(int numerator=0, int denominator=1):
+	m_numerator(numerator), m_denominator(denominator)
+	{
+		// We put reduce() in the constructor to ensure any new fractions we make get reduced!
+		// Any fractions that are overwritten will need to be re-reduced
+		reduce();
+	}
+	
+	// We'll make gcd static so that it can be part of class Fraction without requiring an object of type Fraction to use
+	static int gcd(int a, int b)
+	{
+		return b == 0 ? a : gcd(b, a % b);
+	}
+	
+	void reduce()
+	{
+		if (m_numerator != 0 && m_denominator != 0)
+		{
+			int gcd = Fraction::gcd(m_numerator, m_denominator);
+			m_numerator /= gcd;
+			m_denominator /= gcd;
+		}
+	}
+	
+	friend std::ostream& operator<< (std::ostream& out, const Fraction& f1);
+	friend std::istream& operator>> (std::istream& in, Fraction& f1);
+	
+	friend Fraction operator*(const Fraction &f1, const Fraction &f2);
+	friend Fraction operator*(const Fraction &f1, int value);
+	friend Fraction operator*(int value, const Fraction &f1);
+	
+	void print()
+	{
+		std::cout << m_numerator << "/" << m_denominator << "\n";
+	}
 };
 
-Fraction operator*(const Fraction& f1, const Fraction& f2)
+std::ostream& operator<< (std::ostream& out, const Fraction& f1)
 {
-    return Fraction(f1.m_numerator * f2.m_numerator, f1.m_denominator * f2.m_denominator);
+	out << f1.m_numerator << '/' << f1.m_denominator;
+	return out;
 }
 
-Fraction operator*(const Fraction& f1, int value)
+std::istream& operator>> (std::istream& in, Fraction& f1)
 {
-    return Fraction(f1.m_numerator * value, f1.m_denominator);
+	char slash;
+	in >> f1.m_numerator >> slash >> f1.m_denominator;
+	return in;
 }
 
-Fraction operator*(int value, const Fraction& f1)
+
+// Learncpp's solution
+
+/*
+ std::istream& operator>>(std::istream &in, Fraction &f1)
+ {
+	 // Overwrite the values of f1
+	 in >> f1.m_numerator;
+  
+	 // Ignore the '/' separator
+	 in.ignore(std::numeric_limits<std::streamsize>::max(), '/');
+  
+	 in >> f1.m_denominator;
+  
+	 // Since we overwrite the existing f1, we need to reduce again
+	 f1.reduce();
+  
+	 return in;
+ }
+ */
+
+
+
+Fraction operator*(const Fraction &f1, const Fraction &f2)
 {
-    return Fraction(f1.m_numerator * value, f1.m_denominator);
+	return Fraction(f1.m_numerator * f2.m_numerator, f1.m_denominator * f2.m_denominator);
+}
+
+Fraction operator*(const Fraction &f1, int value)
+{
+	return Fraction(f1.m_numerator * value, f1.m_denominator);
+}
+
+Fraction operator*(int value, const Fraction &f1)
+{
+	return Fraction(f1.m_numerator * value, f1.m_denominator);
 }
 
 int main()
 {
-    Fraction f1(2, 5);
-    f1.print();
-    
-    Fraction f2(3, 8);
-    f2.print();
-    
-    Fraction f3 = f1 * f2;
-    f3.print();
-    
-    Fraction f4 = f1 * 2;
-    f4.print();
-    
-    Fraction f5 = 2 * f2;
-    f5.print();
-    
-    Fraction f6 = Fraction(1, 2) * Fraction(2, 3) * Fraction(3, 4);
-    f6.print();
-    
-    Fraction f7(0, 6);
-    f7.print();
-    
-    
-    return 0;
+	
+	Fraction f1;
+	std::cout << "Enter fraction 1: ";
+	std::cin >> f1;
+	
+	Fraction f2;
+	std::cout << "Enter fraction 2: ";
+	std::cin >> f2;
+	std::cout << f1 << " * " << f2 << " is " << f1 * f2 << '\n'; // note: The result of f1 * f2 is an r-value
+	
+	return 0;
 }
 
-/*
-This should print:
+//Enter fraction 1: 2/3
+//Enter fraction 2: 3/8
+//2/3 * 3/8 is 1/4
 
-2/5
-3/8
-3/20
-4/5
-3/4
-1/4
-0/6
- 
- */
